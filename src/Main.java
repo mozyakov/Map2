24219b0f22dcb35948ea4fc7c150f88217d524c9
mozyakov <s.mozyakov@gmail.com>
2022-10-20 14:15:37 +0700

    ВРоде сделал.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
+    public static final String TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-        System.out.println("Hello hell");
+    public static void main(String[] args) {
+        WordsChecker checker = new WordsChecker(TEXT);
+
+        System.out.println("Cлово Lorem есть в тексте?");
+        System.out.println(checker.hasWord("Lorem"));
+        System.out.println("===================");
+        System.out.println("Слово King есть в тексте?");
+        System.out.println(checker.hasWord("King"));
+
     }
 }
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,2 +1,19 @@
+import java.util.HashSet;
+
 public class WordsChecker {
+    protected String text;
+    protected HashSet<String> words = new HashSet<>();
+
+    public WordsChecker(String text) {
+        this.text = text;
+        String[] sector = text.split("\\P{IsAlphabetic}+");
+        for (String word : sector) {
+            words.add(word);
+        }
+    }
+
+    public boolean hasWord(String wordFromQuery) {
+        return words.contains(wordFromQuery);
+    }
+
 }
